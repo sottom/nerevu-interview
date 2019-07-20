3fe3d322e358c6bce7fc8b855d720ae318d64d77
Mitchell Sotto <sottochoro@gmail.com>
2019-07-20 10:14:44 -0700

    finish basic step 2

--- holiday_scraper.py
+++ holiday_scraper.py
@@ -1,7 +1,10 @@
 import requests
 from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
-from holiday_classes import CurrentYearHolidays
+import csv
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4).pprint
 
 # page = requests.get(f'https://www.timeanddate.com/holidays/us/{datetime.now().year}')
 # soup = BeautifulSoup(page.text, 'lxml')
@@ -15,7 +18,30 @@ from holiday_classes import CurrentYearHolidays
 #     print('Check your scraping')
 
 
-holidays = CurrentYearHolidays()
-print(holidays.getNextTen())
 
+def getNextTen():
+    today = datetime.today()
+    holidays = []
+    next_ten_holidays = []
 
+    with open('holidays.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for i,row in enumerate(csv_reader):
+            if(i > 0 and row[0]):
+                date = datetime.strptime(f'{row[0]}-{today.year}', '%d-%b-%Y')
+                holidays.append({
+                    'date': date,
+                    'date_formatted': date.strftime('%A, %B %d, %Y'),
+                    'name': row[2],
+                    'type': row[3],
+                    'details': row[4]
+                })
+
+    for holiday in holidays:
+        if(holiday['date'] > today and len(next_ten_holidays) < 11):
+            next_ten_holidays.append(holiday)
+
+    return next_ten_holidays
+
+# getNextTen()
+pp(getNextTen())
